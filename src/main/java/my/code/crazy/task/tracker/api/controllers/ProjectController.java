a024a9efd89ff213411e14848810caed8a1f3c89
Nurutdinov <nmi@weblock.su>
2024-12-20 12:47:19 +0300

    Добавлен новый эксэпшен, првоеряющий новые созданные проекты по имени

--- src/main/java/my/code/crazy/task/tracker/api/controllers/ProjectController.java
+++ src/main/java/my/code/crazy/task/tracker/api/controllers/ProjectController.java
@@ -3,7 +3,12 @@ package my.code.crazy.task.tracker.api.controllers;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import my.code.crazy.task.tracker.api.dto.ProjectDto;
+import my.code.crazy.task.tracker.api.exceptions.BadRequestExeption;
 import my.code.crazy.task.tracker.api.factories.ProjectDtoFactory;
+import my.code.crazy.task.tracker.store.repositories.ProjectRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -11,6 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Transactional
 @RestController
 public class ProjectController {
+    private final ProjectRepository projectRepository;
     private final ProjectDtoFactory projectDtoFactory;
-}
 
+    private final String CREATE_PROJECT = "/api/projects";
+
+    @PostMapping(CREATE_PROJECT)
+    public ProjectDto createProject(@RequestParam String name) {
+        projectRepository.findByName(name)
+                         .ifPresent(project -> {
+                             throw new BadRequestExeption(String.format("Project \"%s\" already exists", name));
+                         });
+
+        return null;
+
+        // return projectDtoFactory.makeProjectDto();
+    }
+}
--- src/main/java/my/code/crazy/task/tracker/store/repositories/ProjectRepository.java
+++ src/main/java/my/code/crazy/task/tracker/store/repositories/ProjectRepository.java
@@ -3,5 +3,8 @@ package my.code.crazy.task.tracker.store.repositories;
 import my.code.crazy.task.tracker.store.entities.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+    Optional<ProjectEntity> findByName(String name);
 }
